0259dd0d25ac65b196147657ce6e2838b3101091
卢强 <qiang.lu@renren-inc.com>
2016-03-04 11:27:39 +0800

    Initial Commit

---
+++ LQTouchableAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LQTouchableAnimation
+//
+//  Created by renren on 16/3/4.
+//  Copyright © 2016年 luqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LQTouchableAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LQTouchableAnimation
+//
+//  Created by renren on 16/3/4.
+//  Copyright © 2016年 luqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
